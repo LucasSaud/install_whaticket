7fb59359a180d6baed3ce62393e7626785b8c46a
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2024-09-03 22:14:42 -0300

    Update _frontend.sh

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -16,7 +16,7 @@ frontend_node_dependencies() {
 
   sudo su - deploy <<EOF
   cd /home/deploy/${instancia_add}/frontend
-  npm install
+  npm install --legacy-peer-deps
 EOF
 
   sleep 2
@@ -36,7 +36,7 @@ frontend_node_build() {
 
   sudo su - deploy <<EOF
   cd /home/deploy/${instancia_add}/frontend
-  npm run build
+  npm run build --verbose
 EOF
 
   sleep 2
@@ -59,9 +59,9 @@ frontend_update() {
   pm2 stop ${empresa_atualizar}-frontend
   git pull
   cd /home/deploy/${empresa_atualizar}/frontend
-  npm install
+  npm install --legacy-peer-deps
   rm -rf build
-  npm run build
+  npm run build --verbose
   pm2 start ${empresa_atualizar}-frontend
   pm2 save
 EOF
